66ce38e6501712b5c5fd8412e2171e02bbc41082
Simon Bartha <s.bartha@inray.de>
2024-10-10 13:27:16 +0200

    Add project files.

---
+++ OpenIddictNugetExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace OpenIddictNugetExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenIddictMongoDbBuilder mongoBuilder = null;
+
+            mongoBuilder.UseDatabase(
+                new MongoClient(MongoClientSettings.FromConnectionString("Test"))
+                .GetDatabase("Test")
+            );
+        }
+    }
+}
